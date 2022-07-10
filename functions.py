0da4f4baeecf18f917b156e3459d79aae0dd4327
Cravsi <shaun.bonner1@gmail.com>
2022-07-10 17:06:42 +0000

    [ADD] classes playground

--- functions.py
+++ functions.py
@@ -3,10 +3,9 @@ A playground for functions
 """
 number_list = [12, 45, 60, 87, 999, 200, 85, 77, 99, 20]
 
-"""
-Returns odd numbers from a list of numbers
-"""
+
 def odds(nums):
+    """ Returns odd numbers from a list of numbers """
     odd_nums = [num for num in nums if num % 2 != 0]
     return odd_nums
 
